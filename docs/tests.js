71c732b9ce8dfe90533585d5ce4b0b8c06d647a9
elirudaya <rudayaelinaa@gmail.com>
2026-06-15 00:47:37 +0300

    build metrika

--- docs/tests.js
+++ docs/tests.js
@@ -1,8 +1,8 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 997:
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+/***/ 997
+(module, __unused_webpack_exports, __webpack_require__) {
 
 (function(f){if(true){module.exports=f()}else // removed by dead control flow
 { var g; }})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c=undefined;if(!f&&c)return require(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u=undefined,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
@@ -3761,7 +3761,7 @@ module.exports = Airtable;
 });
 
 
-/***/ })
+/***/ }
 
 /******/ 	});
 /************************************************************************/
@@ -3873,6 +3873,10 @@ const Q_D3LEMON_namespaceObject = __webpack_require__.p + "images/0402b5b0cdf773
 var airtable_umd = __webpack_require__(997);
 var airtable_umd_default = /*#__PURE__*/__webpack_require__.n(airtable_umd);
 ;// ./src/js/tests.js
+/* unused harmony import specifier */ var crossIcon;
+/* unused harmony import specifier */ var testLemon1;
+/* unused harmony import specifier */ var testLemon2;
+/* unused harmony import specifier */ var testLemon3;
 
 
 
